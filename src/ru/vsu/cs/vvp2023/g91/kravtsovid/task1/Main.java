a7ddfac8afb780a6d03095d2147048209b10d5d3
ilya998 <ilya13022006@gmail.com>
2023-09-19 23:57:39 +0300

    input errors fix

--- src/ru/vsu/cs/vvp2023/g91/kravtsovid/task1/Main.java
+++ src/ru/vsu/cs/vvp2023/g91/kravtsovid/task1/Main.java
@@ -4,87 +4,45 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
+    public static void numb(Scanner scn, int d, String n) {
+        try {
+            global.x = scn.nextInt();
+            if (global.x > d) {
+                System.out.println(n + " число выходит за допустимый предел. Пожалуйста вводите только числа от 0 до " + d + ".");
+            } else {
+                global.a -= 1;
+            }
+        } catch (InputMismatchException e) {
+            String err = scn.next();
+            System.out.println('"' + err + '"' + " не является допустимым числом. Пожалуйста вводите только числа от 0 до " + d + ".");
+        }
+    }
+
     public static void main(String[] args) {
-        int a = 3;
-        int h1 = 0;
-        int m1 = 0;
-        int s1 = 0;
-        while (a != 0) {
+        while (global.a != 0) {
+            Scanner scan = new Scanner(System.in);
+            global.a = 3;
             System.out.print("Введите первый момент времени в формате 'часы минуты секунды': ");
-            Scanner scan = new Scanner(System.in);
-            try {
-                h1 = scan.nextInt();
-                if (h1 > 24) {
-                    System.out.println("Количество часов выходит за допустимый предел. Пожалуйста вводите только числа от 0 до 24.");
-                } else {
-                    a = a - 1;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Количество часов выходит за допустимый предел либо вы использовали буквы. Пожалуйста вводите только числа от 0 до 24.");
-            }
-            try {
-                m1 = scan.nextInt();
-                if (m1 > 60) {
-                    System.out.println("Количество минут выходит за допустимый предел. Пожалуйста вводите только числа от 0 до 60.");
-                } else {
-                    a = a - 1;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Количество минут выходит за допустимый предел либо вы использовали буквы. Пожалуйста вводите только числа от 0 до 60.");
-            }
-            try {
-                s1 = scan.nextInt();
-                if (s1 > 60) {
-                    System.out.println("Количество секунд выходит за допустимый предел. Пожалуйста вводите только числа от 0 до 60.");
-                } else {
-                    a = a - 1;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Количество секунд выходит за допустимый предел либо вы использовали буквы. Пожалуйста вводите только числа от 0 до 60.");
-            }
+            numb(scan,24, "Первое");
+            global.h1 = global.x;
+            numb(scan,60, "Второе");
+            global.m1 = global.x;
+            numb(scan,60, "Третье");
+            global.s1 = global.x;
         }
-        a = 3;
-        int h2 = 0;
-        int m2 = 0;
-        int s2 = 0;
-        while (a != 0) {
+        global.a = 3;
+        while (global.a != 0) {
+            global.a = 3;
             System.out.print("Введите второй момент времени в формате 'часы минуты секунды': ");
-            Scanner scan = new Scanner(System.in);
-            try {
-                h2 = scan.nextInt();
-                if (h2 > 24) {
-                    System.out.println("Количество часов выходит за допустимый предел. Пожалуйста вводите только числа от 0 до 24.");
-                } else {
-                    a = a - 1;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Количество часов выходит за допустимый предел либо вы использовали буквы. Пожалуйста вводите только числа от 0 до 24.");
-            }
-            try {
-                m2 = scan.nextInt();
-                if (m2 > 60) {
-                    System.out.println("Количество минут выходит за допустимый предел. Пожалуйста вводите только числа от 0 до 60.");
-                } else {
-                    a = a - 1;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Количество минут выходит за допустимый предел либо вы использовали буквы. Пожалуйста вводите только числа от 0 до 60.");
-            }
-            try {
-                s2 = scan.nextInt();
-                if (s2 > 60) {
-                    System.out.println("Количество секунд выходит за допустимый предел. Пожалуйста вводите только числа от 0 до 60.");
-                } else {
-                    a = a - 1;
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Количество секунд выходит за допустимый предел либо вы использовали буквы. Пожалуйста вводите только числа от 0 до 60.");
-            }
+            Scanner scan1 = new Scanner(System.in);
+            numb(scan1,24, "Первое");
+            global.h2 = global.x;
+            numb(scan1,60, "Второе");
+            global.m2 = global.x;
+            numb(scan1,60, "Третье");
+            global.s2 = global.x;
         }
-        int result = (((h2 + 24) * 3600 + m2 * 60 + s2) - (h1 * 3600 + m1 * 60 + s1)) % 86400;
-        System.out.print("Разница во времени - ");
-        System.out.print(result);
-        System.out.print(" секунд.");
+        int result = (((global.h2 + 24) * 3600 + global.m2 * 60 + global.s2) - (global.h1 * 3600 + global.m1 * 60 + global.s1)) % 86400;
+        System.out.print("Разница во времени - " + result + " секунд.");
     }
 }
-
